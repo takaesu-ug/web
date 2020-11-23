eeb7f4b414665e25894c30ff6608fc351e273b91
Yuji Takaesu <taka.esu.user@gmail.com>
2020-11-24 07:24:05 +0900

    fix: declare inline global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,16 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/theme'
 
 const GlobalStyle = createGlobalStyle`
-  html,
+  html {
+    font-size: 62.5%;
+  }
   body {
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     line-height: 1.4;
-    font-size: ${theme.fontSize};
+    font-size: 1.6rem;
   }
   * {
     box-sizing: border-box;
