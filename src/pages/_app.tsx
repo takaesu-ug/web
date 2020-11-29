ef97558533f112023275247c7efc521f201a576f
Yuji Takaesu <taka.esu.user@gmail.com>
2020-11-29 15:14:18 +0900

    feat: _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,20 @@
+import { CssBaseline } from '@material-ui/core'
+import { StylesProvider, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import { AppProps } from 'next/app'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
 import { theme } from '@/styles/theme'
 
-const GlobalStyle = createGlobalStyle`
-  html {
-    font-size: 62.5%;
-  }
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    line-height: 1.4;
-    font-size: 1.6rem;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  a:hover {
-    text-decoration: underline;
-  }
-  img {
-    max-width: 100%;
-    display: block;
-  }
-`
-
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </>
-)
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </StyledComponentsThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
+  )
+}
 
 export default MyApp
