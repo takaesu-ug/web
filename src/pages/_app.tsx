28015e05dcb8b0311a0fed7c584beab11e46ae4f
Yuji Takaesu <taka.esu.user@gmail.com>
2020-12-13 23:56:41 +0900

    fix: Add GlobalStyle to _app.tsx
    Set html font-size 62.5%(10px), set body font-size 1.6rem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { CssBaseline } from '@material-ui/core'
 import { StylesProvider, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import type { AppProps } from 'next/app'
-import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
+import {
+  createGlobalStyle,
+  ThemeProvider as StyledComponentsThemeProvider,
+} from 'styled-components'
 import { theme } from '@/styles/theme'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-size: 1.6rem;
+  }
+`
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <StylesProvider injectFirst>
     <MuiThemeProvider theme={theme}>
       <StyledComponentsThemeProvider theme={theme}>
         <CssBaseline />
+        <GlobalStyle />
         <Component {...pageProps} />
       </StyledComponentsThemeProvider>
     </MuiThemeProvider>
